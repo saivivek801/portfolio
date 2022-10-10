3d7acf2a7ba97656f3e544da05e38e56765091f9
saivivek <saivivek801@gmail.com>
2022-10-10 22:12:55 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
 <React.StrictMode>
-    <BrowserRouter basename='/saivivek/'>
+    <BrowserRouter basename='/portfolio/'>
     <App />
     </BrowserRouter>
 </React.StrictMode>,
